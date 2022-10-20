c44826e375fd70d5a3854fdd80518552b3a9b6ea
Mykola Shestopal <nick4eva@gmail.com>
2022-10-20 16:54:51 +0300

    Fixed linter errors

--- zsh/autoupdate.zsh
+++ zsh/autoupdate.zsh
@@ -1,3 +1,6 @@
+#!/usr/bin/env zsh
+# vim: set filetype=zsh
+
 # Uncomment the following line to change how often to auto-update (in days).
 export UPDATE_ZSH_DAYS=7
 ZSH_CUSTOM_AUTOUPDATE_QUIET=true
