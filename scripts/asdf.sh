2672088600f5dd462e4c25257892f195857a67e1
Mykola Shestopal <nick4eva@gmail.com>
2022-11-07 17:51:06 +0200

    Added k9s to asdf

--- scripts/asdf.sh
+++ scripts/asdf.sh
@@ -34,6 +34,7 @@ do_configure() {
 	asdf plugin add powershell-core
 	asdf plugin-add broot https://github.com/cmur2/asdf-broot.git
 	asdf plugin-add poetry https://github.com/asdf-community/asdf-poetry.git
+	asdf plugin-add k9s https://github.com/looztra/asdf-k9s
 	info "[asdf][configure] Installing tools"
 	asdf install
 }
